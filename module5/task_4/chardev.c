e4ebc5b1977edb90967c956b187841dffb965a34
particsh <particsh@example.com>
2026-05-10 10:17:52 +0700

    task4

--- module5/task_4/chardev.c
+++ module5/task_4/chardev.c
@@ -8,33 +8,43 @@
 #include <linux/module.h> 
 #include <linux/poll.h> 
  
+
+// объявляем протитпы функций
 static int device_open(struct inode *, struct file *); 
 static int device_release(struct inode *, struct file *); 
 static ssize_t device_read(struct file *, char __user *, size_t, loff_t *); 
 static ssize_t device_write(struct file *, const char __user *, size_t, loff_t *); 
- 
+
+// нужные дефайны, имя файла, длина буффера
 #define SUCCESS 0 
 #define DEVICE_NAME "chardev"  
 #define BUF_LEN 80 
 
+// старшое число устройства 
 static int major; 
+
+// состояния отрытия файла
 enum { 
     CDEV_NOT_USED = 0, 
     CDEV_EXCLUSIVE_OPEN = 1, 
 }; 
  
-static atomic_t already_open = ATOMIC_INIT(CDEV_NOT_USED); 
-static char msg[BUF_LEN]; 
-static struct class *cls;
-static short readPos = 0;
+
+static atomic_t already_open = ATOMIC_INIT(CDEV_NOT_USED);  // состояние файл
+static char msg[BUF_LEN]; // буффер
+static struct class *cls; // 
+static short readPos = 0; // позиция в файле
+
+// функции для взамодействия с файлом
 static struct file_operations chardev_fops = {
-    .owner = THIS_MODULE, 
+    .owner = THIS_MODULE, // счетчик открытия файла
     .read = device_read, 
     .write = device_write, 
     .open = device_open, 
     .release = device_release, 
 }; 
- 
+
+// инизиализация модуля
 static int __init chardev_init(void) { 
     major = register_chrdev(0, DEVICE_NAME, &chardev_fops); 
  
@@ -46,6 +56,7 @@ static int __init chardev_init(void) {
     pr_info("I was assigned major number %d.\n", major); 
  
     cls = class_create(DEVICE_NAME);
+    // добавил защиту от ошибок, возможно это избычточно в контексте задания
     if (IS_ERR(cls)) {
         unregister_chrdev(major, DEVICE_NAME);
         pr_alert("Failed to create class\n");
@@ -53,6 +64,7 @@ static int __init chardev_init(void) {
     }
 
     struct device *dev = device_create(cls, NULL, MKDEV(major, 0), NULL, DEVICE_NAME); 
+    // добавил защиту от ошибок, возможно это избычточно в контексте задания
     if (IS_ERR(dev)) {
         class_destroy(cls);
         unregister_chrdev(major, DEVICE_NAME);
@@ -62,30 +74,33 @@ static int __init chardev_init(void) {
     pr_info("Device created on /dev/%s\n", DEVICE_NAME); 
     return SUCCESS; 
 } 
- 
+
+// выгрузка модуля
 static void __exit chardev_exit(void){ 
     device_destroy(cls, MKDEV(major, 0));
+    // добавил лог об удалении устройства
     pr_info("Device destroyed on /dev/%s\n", DEVICE_NAME); 
     class_destroy(cls); 
- 
     unregister_chrdev(major, DEVICE_NAME); 
 } 
- 
+
+// открытие модуля
 static int device_open(struct inode *inode, struct file *file){ 
-    static int counter = 0; 
- 
+    static int counter = 1; 
+    // чтобы только один процесс одновременно имел доступ к драйверу
     if (atomic_cmpxchg(&already_open, CDEV_NOT_USED, CDEV_EXCLUSIVE_OPEN)) 
         return -EBUSY; 
- 
-    printk(KERN_ALERT "I already told you %d times Hello world!\n", counter++); 
+    pr_info("I already told you %d times Hello world!\n", counter++);
     return SUCCESS; 
 } 
- 
+
+// закрытие модуля
 static int device_release(struct inode *inode, struct file *file){ 
     atomic_set(&already_open, CDEV_NOT_USED); 
     return SUCCESS; 
 } 
- 
+
+// чтение
 static ssize_t device_read(struct file *filp,  
                            char __user *buffer, 
                            size_t length,
@@ -101,7 +116,8 @@ static ssize_t device_read(struct file *filp,
     }
     return count;
 } 
-  
+
+// запись
 static ssize_t device_write(struct file *filp, const char __user *buff, 
                             size_t len, loff_t *off){ 
     short count = 0;
@@ -115,13 +131,8 @@ static ssize_t device_write(struct file *filp, const char __user *buff,
         return -EFAULT;
     }
 
-    memset(msg, 0, sizeof(msg));
+    msg[len] = '\0'; 
     readPos = 0;
-
-    for (ind = len - 1; ind >= 0; ind--) {
-        msg[count++] = kernel_buf[ind];
-    }
-    msg[count] = '\0';
 
     return count;
  
@@ -130,3 +141,5 @@ static ssize_t device_write(struct file *filp, const char __user *buff,
 module_init(chardev_init); 
 module_exit(chardev_exit); 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Плотницкий Максим");
+MODULE_DESCRIPTION("chardev <-> userspace");
